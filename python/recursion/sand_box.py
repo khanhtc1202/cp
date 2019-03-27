68b391fabb3dd03c893805a83b6c77b758fa5599
khanhub <khanh.tran@uzabase.com>
2019-03-27 16:27:15 +0900

    recursion sandbox

---
+++ python/recursion/sand_box.py
@@ -0,0 +1,79 @@
+import time
+import random
+
+def seed_sorted_arr(size, min_value):
+    arr = []
+    counter = 0
+    while counter < size:
+        arr.append(min_value + counter)
+        counter += 1
+    return arr
+
+def seed_rand_arr(size, max_value):
+    arr = []
+    counter = 0
+    while counter < size:
+        arr.append(random.randint(0, max_value))
+        counter += 1
+    return arr
+
+def sum_forward(arr):
+    sum = 0
+    for x in arr:
+        sum += x
+    return sum
+
+def count_DaC(arr):
+    if len(arr) == 1:
+        return 1
+    return 1 + count_DaC(arr[1:])
+
+def binary_search(arr, key):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) / 2
+        if arr[mid] == key:
+            return mid
+        if arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+def sum_DaC(arr):
+    if len(arr) == 1:
+        return arr[0]
+    return arr[0] + sum_DaC(arr[1:])
+
+def max_value_DaC(arr):
+    if len(arr) == 1:
+        return arr[0]
+    return max(arr[0], max_value_DaC(arr[1:]))
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i <= pivot]
+    greater = [i for i in arr[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+def run_time(func, *args):
+    start_time = time.time()
+    print func(*args)
+    end_time = time.time()
+    return end_time - start_time
+
+def main():
+    random_value_arr = seed_rand_arr(1000, 1000)
+    # print("Time taken by DaC %f" % (run_time(sum_DaC, random_value_arr)))
+    # print("Time taken by Loop %f" % (run_time(sum_forward, random_value_arr)))
+    # print("Time taken by DaC %f" % (run_time(count_DaC, random_value_arr)))
+    # sorted_arr = seed_sorted_arr(10, 0)
+    # print("Time taken by Binary %f" % (run_time(binary_search, sorted_arr, 9)))
+    # print("Time taken by DaC %f" % (run_time(max_value_DaC, sorted_arr)))
+    print("Time taken by DaC quick sort %f" % (run_time(quick_sort, random_value_arr)))
+
+if __name__ == "__main__":
+    main()
