2aca04901958060f0d736aaa27b8087a7899826f
khanhub <khanh.tran@uzabase.com>
2019-04-04 23:46:17 +0900

    approximation with greedy

--- python/approximation/greedy.py
+++ python/approximation/greedy.py
@@ -8,9 +8,24 @@ def init():
     stations["kfive"] = set(["ca", "az"])
     return stations, states
 
+def greedy(stations, total_states):
+    final_stations = set()
+    while total_states:
+        best_station = None
+        states_covered = set()
+        for station, states in stations.items():
+            covered = total_states & states
+            if len(covered) > len(states_covered):
+                best_station = station
+                states_covered = covered
+        total_states -= states_covered
+        final_stations.add(best_station)
+    return final_stations
+
 def main():
     stations, states = init()
-    pass
+    best_stations = greedy(stations, states)
+    print best_stations
 
 if __name__ == "__main__":
     main()
