ba5bf6df6648fcefc8dc950a63f712c210951300
khanhtc1202 <khanhtc1202@gmail.com>
2019-11-19 15:40:17 +0900

    remove log

--- atcoder/untags/bestvalue.cpp
+++ atcoder/untags/bestvalue.cpp
@@ -5,13 +5,11 @@ int main() {
 	int round; cin >> round;
 	int cheapest; cin >> cheapest;
 	int best = cheapest >= 0 ? -cheapest : cheapest;
-	cout << "best = " << best << " | cheapest = " << cheapest << "\n";
 	for (int k = 1; k < round; k++) {
 		int val_at_k;
 		cin >> val_at_k;
 		best = max(best, val_at_k - cheapest);
 		cheapest = min(cheapest, val_at_k);
-		cout << "best = " << best << " | cheapest = " << cheapest << "\n";
 	}
 	cout << "max profit => " << best << "\n"; 
 }
