ddf715883d140f80c687316d8299b87aca06b604
khanhtc1202 <khanhtc1202@gmail.com>
2019-11-28 02:02:53 +0900

    binary search with cpp api

--- atcoder/untags/binsearch.cpp
+++ atcoder/untags/binsearch.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 void binary_search(int* array, int n, int key);
 void alternate_binary_search(int* array, int n, int key);
+void count_value_equal(int* arr, int size, int val);
+void binary_search_by_api(int* arr, int size, int val);
 
 int main() {
   int x; cin >> x;
@@ -12,11 +14,16 @@ int main() {
   load_array_int(array, n);
 
   // original way
-//  binary_search(array, n, x);
+  //  binary_search(array, n, x);
  
   // alternate way
-  alternate_binary_search(array, n, x);
+  //  alternate_binary_search(array, n, x);
 
+  // count value equal by api
+  //  count_value_equal(array, n, x);
+
+  // binary search by api
+  binary_search_by_api(array, n, x);
   return 0;
 }
 
@@ -38,4 +45,21 @@ void alternate_binary_search(int* array, int n, int key) {
   if (array[k] == key) {
     cout << "found x at " << k << "\n";
   }
+}
+
+void binary_search_by_api(int* arr, int size, int val) {
+  auto k = lower_bound(arr, arr+size, val) - arr;
+  if (k < size && arr[k] == val)
+    cout << "found x at " << k << "\n";
+}
+
+void count_value_equal(int* arr, int size, int val) {
+  auto a = lower_bound(arr, arr+size, val);
+  auto b = upper_bound(arr, arr+size, val);
+  cout << "number of ele which value eq " << val << " is " << b - a << "\n";
+}
+
+void alternate_count_value_equal(int* arr, int size, int val) {
+  auto r = equal_range(arr, arr+size, val);
+  cout << "number of ele which value eq " << val << " is " << r.second - r.first << "\n";
 }
