d1142a314a06aa146a2ed382c4f2936b35f69120
khanhtc1202 <khanhtc1202@gmail.com>
2021-06-27 00:03:42 +0900

    week7 wip

--- blue/week7/add.cpp
+++ blue/week7/add.cpp
@@ -13,10 +13,14 @@ vector<int> outs;
 
 int main()
 {
-	int n;
-	do
+	int n = 0;
+	while(true)
 	{
 		cin >> n;
+		if (n == 0)
+		{
+			break;
+		}
 
 		int v;
 		for (int i = 0; i < n; i++)
@@ -25,30 +29,28 @@ int main()
 			hp.push(v);
 		}
 
-		int v0 = hp.top();
-		hp.pop();
-		int v1 = hp.top();
-		hp.pop();
-		costs.push_back(v0+v1);
-		while(!hp.empty())
-		{
-			int last = costs[costs.size() - 1];
-			costs.push_back(last + hp.top());
-			hp.pop();
-		}
-
 		int sum = 0;
-		for (int i = 0; i < costs.size(); i++)
+		int cost = 0;
+		int s1, s2;
+		while (hp.size() > 1)
 		{
-			sum += costs[i];
+			s1 = hp.top();
+			hp.pop();
+			s2 = hp.top();
+			hp.pop();
+			sum = s1 + s2;
+			hp.push(sum);
+			cost += sum;
 		}
-		outs.push_back(sum);
+		hp.pop();
 		
-	} while (n != 0);
+		outs.push_back(cost);
+	}
 
 	for (int i = 0; i < outs.size(); i++)
 	{
 		cout << outs[i] << endl;
 	}
+
 	return 0;
 }
---
+++ blue/week7/lazy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <limits>
+using namespace std;
+
+int main()
+{
+	float n = 5;
+	printf("%.2lf\n", n);
+	return 0;
+}
