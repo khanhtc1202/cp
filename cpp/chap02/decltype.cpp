b4a5104a3ad2a04b8ba3daee66da6d5d85e41688
khanhtc1202 <khanhtc1202@gmail.com>
2022-01-02 21:45:34 +0900

    Add example template

--- cpp/chap02/decltype.cpp
+++ cpp/chap02/decltype.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+template<typename R, typename T, typename U>
+R add(T x, U y) {
+    return x+y;
+}
+
+template<typename T, typename U>
+auto add2(T x, U y) -> decltype(x+y) {
+    return x+y;
+}
+
+template<typename T, typename U>
+auto add3(T x, U y){
+    return x + y;
+}
+
 int main() {
     auto x = 1;
     auto y = 2;
@@ -12,5 +27,6 @@ int main() {
     if (std::is_same<decltype(x), decltype(z)>::value)
         std::cout << "type z == type x" << std::endl;
     
+    std::cout << "x+y = " << add2<int, int>(1,2) << std::endl;
     return 0;
 }
