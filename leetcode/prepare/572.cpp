fb2f65fe56b053c1a9625e00cf5b199ddada2b1b
khanhtc1202 <khanhtc1202@gmail.com>
2022-06-23 18:05:55 +0700

    Update

--- leetcode/prepare/572.cpp
+++ leetcode/prepare/572.cpp
@@ -15,7 +15,7 @@ public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if (root == NULL || subRoot == NULL) return root == subRoot;
 
-        if (root->val == subRoot->val && sameTree(root, subRoot))
+        if (root->val == subRoot->val && isSameTree(root, subRoot))
             return true;
         
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
@@ -23,7 +23,7 @@ public:
     }
 
 private:
-    bool sameTree(TreeNode* p, TreeNode* q) {
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         if (p == NULL || q == NULL) return p == q;
         return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
