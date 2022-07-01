bf45c7329abb1644c1b6508ecc67c2febb1f11a2
khanhtc1202 <khanhtc1202@gmail.com>
2022-07-01 21:52:59 +0700

    Add sol

--- leetcode/prepare/103.cpp
+++ leetcode/prepare/103.cpp
@@ -12,5 +12,35 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {}
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (!root) return {};
+
+        vector<vector<int>> out;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool left = true;
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> row(n);
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* tmp = q.front();
+                q.pop();
+
+                if (tmp->left) q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+                
+                if (left) {
+                    row[i] = tmp->val;
+                } else {
+                    row[n - 1 - i] = tmp->val;
+                }
+            }
+            out.push_back(row);
+            left = !left;
+        }
+        return out;
+    }
 };
