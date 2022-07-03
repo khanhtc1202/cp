d4bc4e9864d6fd3e0bbd447d7e358197d89b96ed
khanhtc1202 <khanhtc1202@gmail.com>
2022-07-03 21:19:29 +0700

    Add sol

---
+++ leetcode/prepare/9.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x <= 10)
+            return false;
+
+        int revert = 0;
+        while (x > revert) {
+            revert = revert * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revert || x == revert / 10;
+    }
+};
