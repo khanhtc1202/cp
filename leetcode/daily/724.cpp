ddb22e581245c462fb0d5960c00537fcc7ff34c3
khanhtc1202 <khanhtc1202@gmail.com>
2022-07-17 21:44:02 +0700

    Add sol

--- leetcode/daily/724.cpp
+++ leetcode/daily/724.cpp
@@ -24,4 +24,16 @@ public:
 
         return -1;
     }
+
+    int pivot(vector<int>& nums) {
+        int sum = 0;
+        for (auto& x: nums) sum += x;
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftSum == sum - nums[i] - leftSum)
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
 };
