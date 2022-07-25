2a1f00c2c572d458c920d2d71983a39be43b6792
khanhtc1202 <khanhtc1202@gmail.com>
2022-07-25 21:29:42 +0700

    Add sol

--- leetcode/neetcode/287.cpp
+++ leetcode/neetcode/287.cpp
@@ -4,5 +4,19 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        int slow2 = nums[0];
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
     }
 };
