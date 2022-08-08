d490a94eca22f847a0c2f5529bb7a43e8a54d8da
khanhtc1202 <khanhtc1202@gmail.com>
2022-08-09 00:43:44 +0900

    Add sol

--- leetcode/neetcode/106.cpp
+++ leetcode/neetcode/106.cpp
@@ -12,26 +12,26 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = inorder.size();
-
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = postorder.size();
+        
         unordered_map<int, int> nodes;
         for (int i = 0; i < n; i++)
             nodes[inorder[i]] = i;
-
-        int preorderIdx = 0;
+        
+        int postorderIdx = n-1;
         function<TreeNode*(int, int)> addToTree = [&](int start, int end) -> TreeNode* {
             if (start > end) return nullptr;
-
-            int val = preorder[preorderIdx++];
+            
+            int val = postorder[postorderIdx--];
             TreeNode* root = new TreeNode(val);
-
-            root->left = addToTree(start, nodes[val]-1);
+            
             root->right = addToTree(nodes[val]+1, end);
-
+            root->left = addToTree(start, nodes[val]-1);
+            
             return root;
         };
-
+        
         return addToTree(0, n-1);
     }
 };
