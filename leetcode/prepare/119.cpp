73000b3c180231495aeabc1ea7fd565f2acb3fcd
khanhtc1202 <khanhtc1202@gmail.com>
2022-08-27 23:14:03 +0900

    Add sol

---
+++ leetcode/prepare/119.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans = {1};
+        
+        for (int k = 1; k <= rowIndex; k++)
+            ans.push_back((int)(ans.back() * (long long)(rowIndex - k + 1)/k));
+        
+        return ans;
+    }
+};
