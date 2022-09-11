826e0af09f730ddc497a2d5bb90788af084b85a1
khanhtc1202 <khanhtc1202@gmail.com>
2022-09-11 21:18:45 +0900

    Add sol

---
+++ leetcode/neetcode/1584.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        unordered_set<int> visited;
+        pq.push({0, 0});
+        
+        int ans = 0;
+        while (visited.size() < n) {
+            auto [w, u] = pq.top(); pq.pop();
+            if (visited.find(u) != visited.end()) continue;
+
+            visited.insert(u);
+            ans += w;
+            
+            for (int i = 1; i < n; i++) {
+                if (visited.find(i) != visited.end()) continue;
+                pq.push({abs(points[u][0] - points[i][0])+abs(points[u][1] - points[i][1]), i});
+            }
+        }
+        return ans;
+    }
+};
