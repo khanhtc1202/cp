b31e1fab8b8072e13915b4f759640ee648689ac2
khanhtc1202 <khanhtc1202@gmail.com>
2022-09-16 19:48:45 +0900

    Add sol

---
+++ leetcode/neetcode/1504.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> sum(m, 1);
+            for (int j = i; j < n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    sum[k] *= mat[j][k];
+                    // ans += sum[k] ? ++cnt : (cnt = 0);
+                    if (sum[k]) ans += ++cnt;
+                    else cnt = 0;
+                }
+            }
+        }
+        return ans;
+    }
+};
