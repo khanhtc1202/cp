56b408f2167494757f7177f82c52e03de7377201
khanhtc1202 <khanhtc1202@gmail.com>
2022-09-24 22:31:02 +0900

    Add sol

---
+++ leetcode/prepare/58.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for (int i = s.size()-1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                if (ans == 0) continue;
+                return ans;
+            } else {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
