c6f0774cefa80aaf563005d408c1e57e3101a641
khanhtc1202 <khanhtc1202@gmail.com>
2022-09-30 17:18:15 +0700

    Add sol

---
+++ leetcode/prepare/1334.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, 1e6+1));
+        
+        for (auto e: edges) {
+            int u = e[0], v = e[1], w = e[2];
+            dist[u][v] = dist[v][u] = w;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+        
+        for (int k = 0; k < n; k++)
+            for (int u = 0; u < n; u++)
+                for (int v = 0; v < n; v++) {
+                    if (dist[u][k] + dist[k][v] < dist[u][v]) {
+                        dist[u][v] = dist[u][k] + dist[k][v];
+                    }
+                }
+        
+        int ans = 0, cmin = n+1;
+        for (int u = 0; u < n; u++) {
+            int cnt = 0;
+            for (int v = 0; v < n; v++) {
+                if (dist[u][v] <= distanceThreshold) cnt++;
+            }
+            if (cmin >= cnt) {
+                ans = u;
+                cmin = cnt;
+            }
+        }
+        return ans;
+    }
+};
---
+++ leetcode/prepare/1976.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        int mod = 1e9+7;
+
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto r: roads) {
+            int u = r[0], v = r[1], w = r[2];
+            graph[u].push_back({v,w});
+            graph[v].push_back({u,w});
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long,int>>> heap;
+        heap.push({0, 0});
+        
+        vector<long long> dist(n, 2e11+1); dist[0] = 0;
+        vector<int> count(n, 0); count[0] = 1;
+        
+        while (!heap.empty()) {
+            auto [du, u] = heap.top(); heap.pop();
+            if (du != dist[u]) continue;
+            
+            for (auto [v, wv]: graph[u]) {
+                if (du+wv > dist[v]) continue;
+                if (du+wv == dist[v]) {
+                    count[v] = (count[v] + count[u]) % mod;
+                    continue;
+                }
+                count[v] = count[u];
+                dist[v] = du+wv;
+                heap.push({dist[v], v});
+            }
+        }
+        // for (auto c: count) cout << c << "\n";
+        return count[n-1];
+    }
+};
