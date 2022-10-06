db01ce75bcac8e2f4c2dd646050ac9c6b04014f4
khanhtc1202 <khanhtc1202@gmail.com>
2022-10-06 21:40:24 +0700

    Add sol

---
+++ leetcode/neetcode/290.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> charMap;
+        unordered_map<string, int> strMap;
+        int i = 0, j = 0;
+        int idxi = 0, idxj = 0;
+        while (idxi < pattern.size() && idxj < s.size()) {
+            char c = pattern[idxi]; idxi++;
+            if (charMap.find(c) == charMap.end()) {
+                charMap[c] = i;
+                i++;
+            }
+            string tmp;
+            while (idxj < s.size() && s[idxj] != ' ') {
+                tmp += s[idxj];
+                idxj++;
+            }
+            while (s[idxj] == ' ') idxj++;
+            if (strMap.find(tmp) == strMap.end()) {
+                strMap[tmp] = j;
+                j++;
+            }
+            if (charMap[c] != strMap[tmp]) return false;
+        }
+        return idxi == pattern.size() && idxj == s.size();
+    }
+};
