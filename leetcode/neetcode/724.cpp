f4000e4e426e8c4262187cc1424001b99f4012c9
khanhtc1202 <khanhtc1202@gmail.com>
2022-10-08 22:21:43 +0700

    Add sol

---
+++ leetcode/neetcode/724.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for (auto& x: nums) sum += x;
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftSum == sum - nums[i] - leftSum)
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
