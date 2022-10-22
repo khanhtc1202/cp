7f28dfd4421833ec2aebc8c2f272c4e0740d02b9
khanhtc1202 <khanhtc1202@gmail.com>
2022-10-22 19:15:33 +0700

    Add sol

---
+++ leetcode/prepare/88.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0) return;
+        if (m == 0) {
+            nums1 = nums2;
+            return;
+        }
+        vector<int> tmp;
+        int i1 = 0, i2 = 0;
+        while (i1 < m && i2 < n) {
+            if (nums1[i1] <= nums2[i2]) {
+                tmp.push_back(nums1[i1]);
+                i1++;
+            } else {
+                tmp.push_back(nums2[i2]);
+                i2++;
+            }
+        }
+        
+        if (i1 < m) {
+            while (i1 < m) {
+                tmp.push_back(nums1[i1]);
+                i1++;
+            }
+        }
+        
+        if (i2 < n) {
+            while (i2 < n) {
+                tmp.push_back(nums2[i2]);
+                i2++;
+            }
+        }
+        nums1 = tmp;
+        return;
+    }
+};
