c4d8bd7d84ca6ee008952f5b5e705d85baecea4f
khanhtc1202 <khanhtc1202@gmail.com>
2022-10-25 20:48:37 +0700

    Add sol

--- leetcode/neetcode/179.cpp
+++ leetcode/neetcode/179.cpp
@@ -10,11 +10,11 @@ public:
             tmp[i] = to_string(nums[i]);
         }
         sort(tmp.begin(), tmp.end(), [] (string& s1, string& s2) {
-            return s1 + s2 < s2 + s1;
+            return s1 + s2 > s2 + s1;
         });
         string out = "";
-        if (tmp[sz - 1] == "0") return "0";
-        for (int i = sz - 1; i >= 0; i--) {
+        if (tmp[0] == "0") return "0";
+        for (int i = 0; i < sz; i++) {
             out += tmp[i];
         }
         return out;
