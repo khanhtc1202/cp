19d9765a8e21ca52997b50e3adab22a1768f1e69
khanhtc1202 <khanhtc1202@gmail.com>
2022-11-06 11:41:03 +0700

    Add sol

---
+++ leetcode/neetcode/1438.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        deque<int> maxd, mind;
+        int ans = 0, j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            while(!maxd.empty() && nums[i] >= nums[maxd.back()]) maxd.pop_back();
+            while(!mind.empty() && nums[i] <= nums[mind.back()]) mind.pop_back();
+            
+            maxd.push_back(i); mind.push_back(i);
+            
+            while (nums[maxd.front()] - nums[mind.front()] > limit) {
+                j++;
+                while (maxd.front() < j) maxd.pop_front();
+                while (mind.front() < j) mind.pop_front();
+            }
+            ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+};
