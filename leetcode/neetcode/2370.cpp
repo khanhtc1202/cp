8051d24ef6062f0e49c24c30040787c4de0a7970
khanhtc1202 <khanhtc1202@gmail.com>
2022-11-06 11:40:21 +0700

    Add sol

---
+++ leetcode/neetcode/2370.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        vector<int> maxdp(26, 0);
+        for (auto c: s) {
+            int curr = 0;
+            for (int i = 0; i < 26; i++) {
+                if (abs(c - 'a' - i) > k) continue;
+                
+                curr = max(curr, maxdp[i] + 1);
+            }
+            maxdp[c-'a'] = max(curr, maxdp[c-'a']);
+        }
+        return *max_element(maxdp.begin(), maxdp.end());
+    }
+};
