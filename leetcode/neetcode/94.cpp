64be7a25a040556a158793a0264b1670902043ba
khanhtc1202 <khanhtc1202@gmail.com>
2022-11-10 20:15:36 +0700

    Add sol

---
+++ leetcode/neetcode/94.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        function<void(TreeNode*)> inorder = [&](TreeNode* node) -> void {
+            if (!node) return;
+            
+            inorder(node->left);
+            res.push_back(node->val);
+            inorder(node->right);
+        };
+        
+        inorder(root);
+        return res;
+    }
+};
