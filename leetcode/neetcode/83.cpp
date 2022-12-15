5af09ab35d0810804d7b2407cfb810719447ed45
khanhtc1202 <khanhtc1202@gmail.com>
2022-12-15 14:33:34 +0700

    Add sol

---
+++ leetcode/neetcode/83.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+
+        ListNode *sen = new ListNode();
+        sen->next = head;
+        ListNode *cur = head->next, *prev = head, *toDel = nullptr;
+        while (cur != nullptr) {
+            if (prev->val == cur->val) {
+                toDel = cur;
+                prev->next = cur->next;
+            } else prev = cur;
+
+            cur = cur->next;
+
+            if (toDel != nullptr) {
+                delete toDel;
+                toDel = nullptr;
+            }
+        }
+
+        return sen->next;
+    }
+};
