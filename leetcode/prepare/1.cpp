4b1844a6ea1987c20b7178c4758d6e03eebe93f6
khanhtc1202 <khanhtc1202@gmail.com>
2022-12-17 16:23:25 +0700

    Add sol

---
+++ leetcode/prepare/1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hm;
+        hm[nums[0]] = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (hm.find(target - nums[i]) != hm.end()) {
+                return {hm[target-nums[i]], i};
+            }
+            hm[nums[i]] = i;
+        }
+        return {};
+    }
+};
