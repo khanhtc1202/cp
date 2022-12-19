af46ac5c42f44f129c4ee7ef1f6139aeb8d123ed
khanhtc1202 <khanhtc1202@gmail.com>
2022-12-19 21:21:19 +0700

    Add sol

---
+++ leetcode/neetcode/160.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> nodes_in_B;
+
+        while (headB != nullptr) {
+            nodes_in_B.insert(headB);
+            headB = headB->next;
+        }
+
+        while (headA != nullptr) {
+            if (nodes_in_B.find(headA) != nodes_in_B.end()) {
+                return headA;
+            }
+            headA = headA->next;
+        }
+
+        return nullptr;
+    }
+};
