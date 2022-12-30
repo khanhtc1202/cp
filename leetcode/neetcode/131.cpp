10cdb55ab8c815908704017e48a92769740c9dda
khanhtc1202 <khanhtc1202@gmail.com>
2022-12-30 13:43:56 +0700

    Update sol

--- leetcode/neetcode/131.cpp
+++ leetcode/neetcode/131.cpp
@@ -28,4 +28,36 @@ public:
         backtrack(0);
         return res;
     }
+
+    vector<vector<string>> partitionSlow(string s) {
+        auto palindrome = [&](int l, int r) -> bool {
+            while (l <= r) {
+                if (s[l] != s[r]) return false;
+                l++; r--;
+            }
+            return true;
+        };
+        
+        vector<vector<string>> res;
+        vector<string> tmp;
+        int sz = s.size();
+        
+        function<void(int)> backtrack = [&](int idx) -> void {
+            if (idx >= sz) {
+                res.push_back(tmp);
+                return;
+            }
+            
+            for (int i = idx; i < sz; i++) {
+                if (palindrome(idx, i)) {
+                    tmp.push_back(s.substr(idx, i-idx+1));
+                    backtrack(i+1);
+                    tmp.pop_back();
+                }
+            }
+        };
+        
+        backtrack(0);
+        return res;
+    }
 };
