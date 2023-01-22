09dde9346838bfc7e9d80d303a0cb08b8243b235
khanhtc1202 <khanhtc1202@gmail.com>
2023-01-22 22:09:23 +0700

    Add sol

---
+++ leetcode/prepare/897.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* clone = new TreeNode();
+        TreeNode* curr;
+        curr = clone;
+
+        function<void(TreeNode*)> travel = [&](TreeNode* node) {
+            if (!node) return;
+
+            travel(node->left);
+            node->left = nullptr;
+            curr->right = node;
+            curr = node;
+            travel(node->right);
+        };
+
+        travel(root);
+        return clone->right;
+    }
+};
