7894e13c5c0482a446f8953f1a1d3242e937b7a7
khanhtc1202 <khanhtc1202@gmail.com>
2023-01-30 19:14:17 +0700

    Add sol

---
+++ leetcode/prepare/1008.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int idx = 0;
+        int n = preorder.size();
+
+        function<TreeNode*(int, int)> build = [&](int low, int high) -> TreeNode* {
+            if (idx == n) return nullptr;
+
+            int val = preorder[idx];
+            if (val < low || val > high) return nullptr;
+
+            idx++;
+            TreeNode* node = new TreeNode(val);
+            node->left = build(low, val);
+            node->right = build(val, high);
+            return node;
+        };
+
+        return build(0, 1001);
+    }
+};
