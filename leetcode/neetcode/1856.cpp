27490416c9abcc958a40482ad00469832291009b
khanhtc1202 <khanhtc1202@gmail.com>
2023-04-02 17:59:57 +0700

    Add sol

--- leetcode/neetcode/1856.cpp
+++ leetcode/neetcode/1856.cpp
@@ -1,20 +1,44 @@
+/*
+https://leetcode.com/problems/maximum-subarray-min-product/
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     int maxSumMinProduct(vector<int>& nums) {
-        int ans = 0; int k = 1e9+7;
-        for (int i = 0; i < nums.size(); i++) {
-            int cmin = nums[i];
-            int csum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                cmin = min(cmin, nums[j]);
-                csum += nums[j];
-                csum %= k;
-                ans = max(ans, (cmin * csum)%k);
+        int n = nums.size();
+        vector<long long> prefix(n);
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++)
+            prefix[i] = (prefix[i-1] + nums[i]);
+
+        vector<int> L(n); L[0] = 0;
+        stack<int> stk;
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && nums[stk.top()] >= nums[i]) {
+                stk.pop();
             }
+            L[i] = stk.empty() ? 0 : stk.top() + 1;
+            stk.push(i);
         }
-        return ans;
+
+        vector<int> R(n); R[n-1] = n-1;
+        stk = stack<int> ();
+        for (int i = n-1; i > -1; i--) {
+            while (!stk.empty() && nums[stk.top()] >= nums[i]) {
+                stk.pop();
+            }
+            R[i] = stk.empty() ? n-1 : stk.top() - 1;
+            stk.push(i);
+        }
+
+        long long res = 0;
+        for (int i = 0; i < n; i++) {
+            res = max(res, nums[i] * (prefix[R[i]] - prefix[L[i]] + nums[L[i]]));
+        }
+        int mod = 1e9 + 7;
+        return res % mod;
     }
 };
