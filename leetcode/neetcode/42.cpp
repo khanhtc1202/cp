18186e20afdbaf9c66755ddb934ffc8967e318c9
khanhtc1202 <khanhtc1202@gmail.com>
2023-04-04 17:43:30 +0700

    Add sol

--- leetcode/neetcode/42.cpp
+++ leetcode/neetcode/42.cpp
@@ -23,4 +23,29 @@ public:
         }
         return res;
     }
+
+    int _trap(vector<int>& height) {
+        int n = height.size();
+        int res = 0;
+        int l = 0, r = n-1;
+        int lmax = height[0], rmax = height[n-1];
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lmax) {
+                    lmax = height[l];
+                } else {
+                    res += (lmax - height[l]);
+                }
+                l++;
+            } else {
+                if (height[r] >= rmax) {
+                    rmax = height[r];
+                } else {
+                    res += (rmax - height[r]);
+                }
+                r--;
+            }
+        }
+        return res;
+    }
 };
