c52a50615bcae689c35c43ea1a35775589f7f132
khanhtc1202 <khanhtc1202@gmail.com>
2023-04-13 13:36:12 +0700

    Add sol

--- leetcode/prepare/844.cpp
+++ leetcode/prepare/844.cpp
@@ -1,3 +1,6 @@
+/*
+https://leetcode.com/problems/backspace-string-compare/
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,5 +25,43 @@ public:
             stk.pop();
         }
         return out;
+    }
+
+    bool backspaceCompare_O1Space(string s, string t) {
+        int i = s.size() - 1, j = t.size() - 1;
+        int skipS = 0, skipT = 0;
+        while (i >= 0 || j >= 0) {
+            // Skip # and its "follow" alphabet char from s until get comparable char.
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS) {
+                    skipS--;
+                    i--;
+                } else break;
+            }
+            // Same in t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT) {
+                    skipT--;
+                    j--;
+                } else break;
+            }
+
+            // The two comparable chars are not equal.
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+
+            // One of string reaches the end but the other is not.
+            if ((i >= 0) != (j >= 0))
+                return false;
+
+            i--; j--;
+        }
+        return true;
     }
 };
