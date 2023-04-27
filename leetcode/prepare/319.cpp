428467ec66a1cd293b2db8dff91ad834768959b2
khanhtc1202 <khanhtc1202@gmail.com>
2023-04-27 17:19:05 +0700

    Add sol

---
+++ leetcode/prepare/319.cpp
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/bulb-switcher/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int buldSwitch(int n) {
+        return sqrt(n);
+    }
+};
