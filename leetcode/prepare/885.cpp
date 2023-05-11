b06b62da594b88eee61465ace13c007455686076
khanhtc1202 <khanhtc1202@gmail.com>
2023-05-11 12:19:56 +0700

    Update sol

--- leetcode/prepare/885.cpp
+++ leetcode/prepare/885.cpp
@@ -14,32 +14,28 @@ public:
 
         while (ans.size() < rows*cols) {
             if (up >= 0) {
-                for (int col = left+1; col <= right; col++) {
-                    if (col < 0) continue;
+                for (int col = max(0, left+1); col <= right; col++) {
                     if (col >= cols) break;
                     ans.push_back({up, col});
                 }
             }
 
             if (right < cols) {
-                for (int row = up+1; row <= down; row++) {
-                    if (row < 0) continue;
+                for (int row = max(0, up+1); row <= down; row++) {
                     if (row >= rows) break;
                     ans.push_back({row, right});
                 }
             }
 
             if (down < rows) {
-                for (int col = right-1; col >= left; col--) {
-                    if (col >= cols) continue;
+                for (int col = min(cols-1, right-1); col >= left; col--) {
                     if (col < 0) break;
                     ans.push_back({down, col});
                 }
             }
 
             if (left >= 0) {
-                for (int row = down-1; row >= up; row--) {
-                    if (row >= rows) continue;
+                for (int row = min(rows-1, down-1); row >= up; row--) {
                     if (row < 0) break;
                     ans.push_back({row, left});
                 }
