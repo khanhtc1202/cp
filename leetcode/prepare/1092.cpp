138f8a36fde9e76183e5065b16e28e5dc9eb0b8d
khanhtc1202 <khanhtc1202@gmail.com>
2023-05-12 13:46:49 +0700

    Add sol

--- leetcode/prepare/1092.cpp
+++ leetcode/prepare/1092.cpp
@@ -14,9 +14,10 @@ public:
             for (int j = 0; j <= m; j++) {
                 if (i == 0 || j == 0) dp[i][j] = max(i, j);
                 else {
-                    dp[i][j] = min({dp[i-1][j-1]+2, dp[i-1][j]+1, dp[i][j-1]+1});
                     if (str1[i-1] == str2[j-1])
-                        dp[i][j] = min(dp[i][j], dp[i-1][j-1] + 1);
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    else
+                        dp[i][j] = min({dp[i-1][j-1]+2, dp[i-1][j]+1, dp[i][j-1]+1});
                 }
             }
 
