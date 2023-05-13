631706652baecfe734d80748beef405ef9ef79b5
khanhtc1202 <khanhtc1202@gmail.com>
2023-05-13 18:23:37 +0700

    Update sol

--- leetcode/prepare/1547.cpp
+++ leetcode/prepare/1547.cpp
@@ -7,12 +7,10 @@ using namespace std;
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
-        
         cuts.push_back(0); cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
 
         int sz = cuts.size();
-        // vector<vector<int>> dp(sz, vector<int>(sz));
         int dp[103][103]{};
 
         for (int len = 2; len < sz; len++) {
@@ -23,6 +21,25 @@ public:
                 }
             }
         }
+        return dp[0][sz-1];
+    }
+
+    int minCostOp(int n, vector<int>& cuts) {
+        cuts.push_back(0); cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+
+        int sz = cuts.size();
+        int dp[103][103]{};
+
+        for (int i = sz-1; i >= 0; i--) {
+            for (int j = i+2; j < sz; j++) {
+                dp[i][j] = INT_MAX;
+                for (int k = i+1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + cuts[j] - cuts[i]);
+                }
+            }
+        }
+
         return dp[0][sz-1];
     }
 };
