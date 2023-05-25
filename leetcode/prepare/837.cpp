7fb17047ea48c7eb65080438c8bd2c82c44f93bc
khanhtc1202 <khanhtc1202@gmail.com>
2023-05-25 22:08:28 +0700

    Add sol

---
+++ leetcode/prepare/837.cpp
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/new-21-game/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        vector<double> dp(n+1);
+        dp[0] = 1;
+        double s = k > 0 ? 1 : 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = s / maxPts;
+            if (i < k) {
+                s += dp[i];
+            }
+            if (i - maxPts >= 0 && i - maxPts < k) {
+                s -= dp[i - maxPts];
+            }
+        }
+        return accumulate(dp.begin() + k, dp.end(), 0.0);
+    }
+};
