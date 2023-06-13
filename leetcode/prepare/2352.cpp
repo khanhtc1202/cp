dba608219fa616d0735a81cfb0a61bedd82195e3
khanhtc1202 <khanhtc1202@gmail.com>
2023-06-13 10:15:25 +0700

    Update sol

--- leetcode/prepare/2352.cpp
+++ leetcode/prepare/2352.cpp
@@ -27,4 +27,23 @@ public:
         }
         return ans;
     }
+
+    int equalPairsMap(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        map<vector<int>, int> row2cnt;
+        for(int i=0;i<n;i++)
+            row2cnt[grid[i]]++;
+        
+        int cnt = 0;
+        vector<int> col(n);
+        for(int j = 0; j < n; j++) {
+            for(int i = 0; i < n; i++)
+                col[i] = grid[i][j];
+            
+            cnt += row2cnt[col] ;
+        }
+
+        return cnt;
+    }
 };
