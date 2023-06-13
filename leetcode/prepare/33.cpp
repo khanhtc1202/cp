38ac91d710c8ced69207925a914c8a2f70ff10de
khanhtc1202 <khanhtc1202@gmail.com>
2023-06-13 18:27:36 +0700

    Update sol

--- leetcode/prepare/33.cpp
+++ leetcode/prepare/33.cpp
@@ -45,4 +45,27 @@ public:
             else return binarySearch(nums, 0, pivot-1, target);
         }
     }
+
+    int searchOpt(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (nums[mid] == target) return mid;
+
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && nums[mid] > target) {
+                    r = mid-1;
+                } else {
+                    l = mid+1;
+                }
+            } else {
+                if (nums[mid] < target && nums[r] >= target) {
+                    l = mid+1;
+                } else {
+                    r = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
 };
