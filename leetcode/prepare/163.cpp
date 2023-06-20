d8d34a3a9d71e5c0d8602a15f211153e09542c4c
khanhtc1202 <khanhtc1202@gmail.com>
2023-06-20 11:16:29 +0700

    Add sol

---
+++ leetcode/prepare/163.cpp
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/missing-ranges/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (lower < nums[i]) {
+                ans.push_back({lower, nums[i]-1});
+            }
+            lower = nums[i]+1;
+        }
+        if (lower <= upper) {
+            ans.push_back({lower, upper});
+        }
+        return ans;
+    }
+};
