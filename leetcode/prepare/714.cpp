e9969d295ae88cc3efd208eabd892c61bb20d0f9
khanhtc1202 <khanhtc1202@gmail.com>
2023-06-24 11:41:30 +0700

    Update sol

--- leetcode/prepare/714.cpp
+++ leetcode/prepare/714.cpp
@@ -18,4 +18,35 @@ public:
         }
         return free;
     }
+
+    int maxProfitSlow(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<int> dp(n, 0);
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i-1];
+            for (int j = 0; j < i; j++) {
+                int prev = j ? dp[j-1] : 0;
+                dp[i] = max(dp[i], prev + prices[i] - prices[j] - fee);
+            }
+        }
+        return dp[n-1];
+    }
+
+    int maxProfitOpt(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<int> dp(n, 0);
+        int curMax = -prices[0]; // buy at 0;
+
+        // dp[i] = max(dp[i], dp[j-1] + prices[i] - prices[j] - fee)
+        // => dp[i] = max(dp[i], dp[j-1] - prices[j] + prices[i] - fee)
+        // since (prices[i] - fee) is const, find max(dp[j-1] - prices[j])
+        for (int i = 0; i < n; i++) {
+            if (i) dp[i] = dp[i - 1];
+
+            dp[i] = max(dp[i], curMax + prices[i] - fee);
+            curMax = max(curMax, (i ? dp[i - 1] : 0) - prices[i]);
+        }
+
+        return dp[n - 1];
+    }
 };
