8ec16b7740c87554a34a45d846bf9c8bc54bad51
khanhtc1202 <khanhtc1202@gmail.com>
2023-07-14 15:41:02 +0700

    Add sol

---
+++ leetcode/prepare/1218.cpp
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int diff) {
+        unordered_map<int, int> dp;
+        int ans = 0;
+        for (int i = 0; i < arr.size(); ++i) {
+            dp[arr[i]] = dp[arr[i] - diff] + 1;
+            ans = max(ans, dp[arr[i]]);
+        }
+        return ans;
+    }
+};
