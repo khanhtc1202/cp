61bdac4995c0f9fbdc89be22e9c0be59a8c8e1f5
khanhtc1202 <khanhtc1202@gmail.com>
2023-08-15 17:57:24 +0700

    Add sol

---
+++ leetcode/prepare/1290.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+        while (head) {
+            res = res * 2 + head->val;
+            head = head->next;
+        }
+        return res;
+    }
+};
