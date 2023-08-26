bfb00ddc718f0d0d0c8df68a4ece264cf99ee491
khanhtc1202 <khanhtc1202@gmail.com>
2023-08-26 21:03:13 +0700

    Update sol

--- leetcode/prepare/646.cpp
+++ leetcode/prepare/646.cpp
@@ -10,9 +10,9 @@ public:
         sort(pairs.begin(), pairs.end());
         vector<pair<int, int>> dp;
         dp.push_back(make_pair(0, -1e4));
-        for (auto p: pairs) {
+        for (const auto& p: pairs) {
             pair<int, int> last = make_pair(0, -1e4);
-            for (auto prev: dp) {
+            for (const auto& prev: dp) {
                 if (p[0] > prev.second) {
                     last = max(last, make_pair(prev.first+1, p[1]));
                 }
@@ -20,5 +20,20 @@ public:
             dp.push_back(last);
         }
         return max_element(dp.begin(), dp.end())->first;
+    }
+
+    int findLongestChain_Greedy(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        
+        int curr = INT_MIN, ans = 0;
+        for (const auto& p: pairs) {
+            if (p[0] > curr) {
+                ans++;
+                curr = p[1];
+            }
+        }
+        return ans;
     }
 };
