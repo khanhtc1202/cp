aa5f14a15ea02b60746b719bee53f77da628afee
khanhtc1202 <khanhtc1202@gmail.com>
2023-09-06 15:39:16 +0700

    Add sol

---
+++ leetcode/prepare/725.cpp
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/split-linked-list-in-parts/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        int cnt = 0;
+        ListNode* curr = head;
+        while (curr) {
+            cnt++;
+            curr = curr->next;
+        }
+
+        int sz = cnt / k;
+        int extra = cnt % k;
+        vector<ListNode*> ans(k, {});
+        
+        curr = head;
+        ListNode* prev = new ListNode();
+        prev->next = curr;
+
+        for (int i = 0; i < k; ++i) {
+            int iLen = sz + (extra-- > 0 ? 1 : 0);
+            if (!curr) break; // stop immediately if there is no more node
+            ans[i] = curr;
+            iLen--;
+            prev = curr; curr = curr->next;
+            while (iLen && curr) {
+                prev = curr;
+                curr = curr->next;
+                iLen--;
+            }
+            prev->next = nullptr;
+        }
+        return ans;
+    }
+};
