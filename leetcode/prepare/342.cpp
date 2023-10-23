b67f580d81403730adedabea0efab225325362ce
khanhtc1202 <khanhtc1202@gmail.com>
2023-10-23 17:36:06 +0700

    Add sol

---
+++ leetcode/prepare/342.cpp
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/power-of-four/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        return n>0 && !(n & (n-1)) && !(n & 0x2aaaaaaa);
+    }
+};
