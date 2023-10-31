e90b257574a809fde0a952836f84f2b47fe8c2bb
khanhtc1202 <khanhtc1202@gmail.com>
2023-10-31 19:35:34 +0700

    Add sol

---
+++ leetcode/prepare/2433.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int n = pref.size();
+        for (int i = n-1; i > 0; --i) {
+            pref[i] = pref[i] ^ pref[i-1];
+        }
+        return pref;
+    }
+};
