934d935776b3270802091e86ccc097cfb27b6054
khanhtc1202 <khanhtc1202@gmail.com>
2023-11-25 22:19:39 +0700

    Update sol

--- leetcode/prepare/1685.cpp
+++ leetcode/prepare/1685.cpp
@@ -13,7 +13,8 @@ public:
 
         vector<int> ans(n, 0);
         for (int i = 0; i < n; ++i) {
-            // (nums[i] * i - (nums[0] + nums[1] + ... + nums[i-1])) + ((nums[i+1] + nums[i+2] + ... + nums[n-1]) - nums[i] * (n-i-1))
+            // (nums[i] * i - (nums[0] + nums[1] + ... + nums[i-1])) + ((nums[i] + nums[i+1] + nums[i+2] + ... + nums[n-1]) - nums[i] * (n-i))
+            // (nums[i] * i - left) + (right - nums[i] * (n-i))
             ans[i] = (2*i - n) * nums[i] + right - left;
             left += nums[i];
             right -= nums[i];
