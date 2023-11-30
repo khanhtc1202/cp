258c12752b93c81d6ac10aa55922c375876083f1
khanhtc1202 <khanhtc1202@gmail.com>
2023-11-30 18:26:35 +0700

    Add sol

---
+++ leetcode/prepare/1611.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if (n == 0) return 0;
+
+        int k = 0;
+        int curr = 1;
+        while (curr * 2 <= n) {
+            curr *= 2;
+            k++;
+        }
+
+        return (1 << (k+1)) - 1 - minimumOneBitOperations(n^curr);
+    }
+};
