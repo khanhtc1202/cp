e6b25d7a46b67e11b763ff461c938a30223fab87
khanhtc1202 <khanhtc1202@gmail.com>
2023-12-05 17:42:12 +0700

    Add sol

---
+++ leetcode/prepare/1688.cpp
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/count-of-matches-in-tournament/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
