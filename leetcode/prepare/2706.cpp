ac5ced720294e9f09bfcbbda1d371c9de3d65432
khanhtc1202 <khanhtc1202@gmail.com>
2023-12-20 12:06:48 +0700

    Add sol

---
+++ leetcode/prepare/2706.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/buy-two-chocolates/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for (auto p: prices) {
+            if (p < min1) {
+                min2 = min1;
+                min1 = p;
+            } else if (p < min2) {
+                min2 = p;
+            }
+        }
+        return min1 + min2 > money ? money : money - (min1+min2);
+    }
+};
