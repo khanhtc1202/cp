a3b336b39de7f45a32c0743c4ee9b760cddf9a58
khanhtc1202 <khanhtc1202@gmail.com>
2024-01-06 21:49:21 +0700

    Add sol

--- leetcode/prepare/1235.cpp
+++ leetcode/prepare/1235.cpp
@@ -6,6 +6,35 @@ using namespace std;
 
 class Solution {
 public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit){
+        int n = profit.size();
+        vector<tuple<int, int, int>> jobs(n);
+
+        for (int i = 0; i < n; ++i)
+            jobs[i] = {endTime[i], startTime[i], profit[i]};
+
+        sort(jobs.begin(), jobs.end());
+        vector<int> dp(n+1);
+
+        for (int i = 0; i < n; ++i) {
+            auto [endTime, startTime, profit] = jobs[i];
+            int firstJBeforeI = upper_bound(jobs.begin(), jobs.begin()+i, startTime,
+                // Find the first job that end before starting the job at startTime
+                [&](int time, const auto& job) -> bool {
+                    return time < get<0>(job);
+                }) - jobs.begin();
+
+            // Update (i+1) by compare
+            // - do task job[i] = dp[firstJBeforeI]+profit_of_i
+            // - do not do task job[i] = dp[i]
+            dp[i+1] = max(dp[i], dp[firstJBeforeI]+profit);
+        }
+        return dp[n];
+    }
+};
+
+class Solution1 {
+public:
     using int3 = array<int, 3>;
     vector<int3> jobs; // (startTime, endTime, profit)
     vector<int> dp;
