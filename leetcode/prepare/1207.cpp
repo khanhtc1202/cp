e8e4612e31d044382cc5b197761f3a6b1dc00c5c
khanhtc1202 <khanhtc1202@gmail.com>
2024-01-18 09:26:47 +0700

    Add sol

---
+++ leetcode/prepare/1207.cpp
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/unique-number-of-occurrences/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int freq[2001]={0};
+        for (int x: arr){
+            freq[x+1000]++;
+        }
+        bool S[1001]={0};
+        for (int f: freq){
+            if (f>=1 && S[f]==1) 
+                return 0;
+            S[f]=1;
+        }
+        return 1;
+    }
+};
