0a2b75963d5a6e37bfb422fc929a111abf1d6b13
khanhtc1202 <khanhtc1202@gmail.com>
2024-03-01 09:39:19 +0700

    Add sol

---
+++ leetcode/prepare/2864.cpp
@@ -0,0 +1,14 @@
+/*
+https://leetcode.com/problems/maximum-odd-binary-number/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int one = count(s.begin(), s.end(), '1');
+        int zero = s.size() - one;
+        return string(one - 1, '1') + string(zero, '0') + '1';
+    }
+};
