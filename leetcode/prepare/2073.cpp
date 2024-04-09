a61b07b6cba4cf5575756fd1283e957a7bfd912e
khanhtc1202 <khanhtc1202@gmail.com>
2024-04-09 22:28:11 +0700

    Add sol

---
+++ leetcode/prepare/2073.cpp
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/time-needed-to-buy-tickets/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans = 0;
+        for (int i = 0; i < tickets.size(); ++i) {
+            if (i <= k) {
+                ans += min(tickets[k], tickets[i]);
+            } else {
+                ans += min(tickets[k]-1, tickets[i]);
+            }
+        }
+        return ans;
+    }
+};
