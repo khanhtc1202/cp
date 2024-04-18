076124dedf44a65460b1ab6bba0b65bdc15b7e48
khanhtc1202 <khanhtc1202@gmail.com>
2024-04-18 10:45:29 +0700

    Update sol

--- leetcode/prepare/463.cpp
+++ leetcode/prepare/463.cpp
@@ -28,4 +28,25 @@ public:
         }
         return ans;
     }
+
+    int islandPerimeter_novisited(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0; queue<pair<int,int>> q;
+        vector<int> dx = {1,-1,0,0};
+        vector<int> dy = {0,0,-1,1};
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    ans += 4;
+                    for (int k = 0; k < 4; ++k) {
+                        int u = i + dx[k], v = j + dy[k];
+                        if (u < 0 || u >= n || v < 0 || v >= m) continue;
+                        if (grid[u][v] == 1) ans -= 1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 };
