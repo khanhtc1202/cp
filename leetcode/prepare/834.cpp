58403c54a18b58cd0624cce43a0edb33c9e7cdb9
khanhtc1202 <khanhtc1202@gmail.com>
2024-04-28 19:17:38 +0700

    Add sol

---
+++ leetcode/prepare/834.cpp
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/sum-of-distances-in-tree/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    vector<vector<int>> adj;
+    vector<int> sum, cnt;
+public:
+    void build_adj(int n, vector<vector<int>>& edges) {
+        adj.resize(n);
+        for (auto& e : edges) {
+            int v = e[0], w = e[1];
+            adj[v].push_back(w);
+            adj[w].push_back(v);
+        }
+    }
+
+    void dfs(int i, int parent=-1){
+        for(int j: adj[i]){
+            if (j==parent) continue;
+            dfs(j, i);
+            cnt[i]+=cnt[j];
+            sum[i]+=sum[j]+cnt[j];//subtree for root=i
+        }
+    }
+
+    void reroot(int i, int parent, int n) {
+        for (int j : adj[i]) {
+            if (j == parent)
+                continue;
+            sum[j]=sum[i]+n-2*cnt[j];
+            reroot(j, i, n);
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        build_adj(n, edges);
+        sum.assign(n, 0);
+        cnt.assign(n, 1);
+        dfs(0);
+        reroot(0, -1, n);
+        return sum;
+    }
+};
