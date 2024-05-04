1ecd3123bedea79f9bc7cee793541cc5431e2d31
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2024-05-04 10:58:09 +0700

    Add sol

---
+++ leetcode/prepare/881.cpp
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/boats-to-save-people/
+*/
+#include <bits/stdc.h>
+using namespace std;
+
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        unsigned freq[30001]={0};
+        int maxW=0, minW=30001;
+        for(int x: people){
+            freq[x]++;
+            maxW=max(maxW, x);
+            minW=min(minW, x);
+        }
+        for (int i=minW, j=0; i<=maxW; i++){
+            int f=freq[i];
+            fill(people.begin()+j, people.begin()+j+f, i);
+            j+=f;
+        }      
+        int x=0;
+        for(int l=0, r=people.size()-1;l<=r; r--){
+            x++;
+            cout<<"boat "<<x<<":";
+            cout<<people[r]<<",";
+            if (people[l]+people[r]<=limit){
+                cout<<people[l];
+                l++; 
+            }
+            cout<<endl;         
+        }
+        return x;
+    }
+};
