521a3582fe20db7ae2cdcbbcfa3f6a9379e8bdae
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2024-05-07 21:34:53 +0700

    Add sol

---
+++ leetcode/prepare/2816.cpp
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/
+*/
+#include <bits/stdc.h>
+using namespace std;
+#pragma GCC optimize("O3", "unroll-loops")
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+}
+
+class Solution {
+public:
+    static  ListNode* doubleIt(ListNode* head) {
+        ListNode* prev= NULL;
+        if (head->val>=5)
+            head= new ListNode(0,head);
+        prev=head;
+        for (ListNode* curr=head; curr; prev=curr, curr=curr->next){
+            if (curr->val>=5){
+                prev->val++; 
+                curr->val=curr->val*2-10;
+            }
+            else 
+               curr->val*=2;
+                
+        }
+        return head;
+    }
+};
