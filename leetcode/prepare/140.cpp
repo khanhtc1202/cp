1d2c392b609eba11f9982d76cbbf8a2e2cb01c2e
khanhtc1202 <khanhtc1202@gmail.com>
2024-05-25 21:05:48 +0700

    Update sol

--- leetcode/prepare/140.cpp
+++ leetcode/prepare/140.cpp
@@ -7,13 +7,16 @@ using namespace std;
 class Solution {
     void backtrack(const string& s, const unordered_set<string>& wordSet, string& currentSentence,
                     vector<string>& results, int startIndex) {
+        // Reach the end of the string then current sentence is a valid sentence
         if (startIndex == s.length()) {
             results.push_back(currentSentence);
             return;
         }
 
+        // Iterate over possible end indices
         for (int endIndex = startIndex+1; endIndex <= s.length(); ++endIndex) {
             string word = s.substr(startIndex, endIndex - startIndex);
+            // If the word is in the set, proceed with backtracking
             if (wordSet.find(word) != wordSet.end()) {
                 string originalSentence = currentSentence;
                 if (!currentSentence.empty()) currentSentence += " ";
