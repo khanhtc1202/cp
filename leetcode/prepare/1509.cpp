a19a16875f14869063e4810d8ca1596bb3fd52d2
khanhtc1202 <khanhtc1202@gmail.com>
2024-07-03 16:38:26 +0700

    Update sol

--- leetcode/prepare/1509.cpp
+++ leetcode/prepare/1509.cpp
@@ -9,7 +9,8 @@ public:
     int minDifference(vector<int>& nums) {
         if (nums.size() <= 4) return 0;
         sort(nums.begin(), nums.end());
-        int ans = INT_MAX, N = nums.size();
+        int N = nums.size();
+        int ans = nums[N-1] - nums[0];
         for (int i = 0; i <= 3; ++i) {
             ans = min(ans, nums[N-1-i] - nums[3-i]);
         }
