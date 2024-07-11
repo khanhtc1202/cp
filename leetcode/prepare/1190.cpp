75e793f494e8acd28d13661d1cf7b0831cf76e1e
khanhtc1202 <khanhtc1202@gmail.com>
2024-07-11 16:56:03 +0700

    Update sol

--- leetcode/prepare/1190.cpp
+++ leetcode/prepare/1190.cpp
@@ -23,4 +23,35 @@ public:
         }
         return result;
     }
+
+    string reverseParentheses_Opt(string s) {
+        int N = s.size();
+        vector<int> opened;
+        vector<int> pair(N);
+
+        for (int i = 0; i < N; ++i) {
+            if (s[i] == '(') {
+                opened.push_back(i);
+            }
+            if (s[i] == ')') {
+                int j = opened.back();
+                opened.pop_back();
+                // create the wormhole
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+
+        string ans;
+        // direction == 1 mean travel forward, == -1 mean travel backward
+        for (int curr = 0, direction = 1; curr < N; curr += direction) {
+            if (s[curr] == '(' || s[curr] == ')') {
+                curr = pair[curr]; // jump to the connected parenthes
+                direction = -direction;
+            } else {
+                ans += s[curr];
+            }
+        }
+        return ans;
+    }
 };
