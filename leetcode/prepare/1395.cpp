fc89a88fd4147e1314446078c6ef74ef4479881b
khanhtc1202 <khanhtc1202@gmail.com>
2024-07-29 16:08:40 +0700

    Update sol

--- leetcode/prepare/1395.cpp
+++ leetcode/prepare/1395.cpp
@@ -31,4 +31,51 @@ public:
         }
         return ans;
     }
+
+    int numTeams_Opt(vector<int>& rating) {
+        const int n=rating.size();
+        const int m=*max_element(rating.begin(), rating.end());
+
+        FenwickTree L(m+1), R(m+1);
+
+        for(int x: rating) R.update(x, 1);
+
+        int cnt=0;
+        for(int x: rating){
+            R.update(x, -1);// remove x from R
+
+            int LLess=L.sum(x-1);
+            int RLess=R.sum(x-1);
+            int LBigger=L.sum(x+1, m);
+            int RBigger=R.sum(x+1, m);
+
+            cnt+=LLess*RBigger+LBigger*RLess;
+
+            L.update(x, 1);// add x to L
+        }
+        return cnt;
+    }
+};
+
+// Or Binary Indexes Tree (BIT)
+struct FenwickTree{
+    vector<int> BIT;
+    int n;
+    FenwickTree(int n): n(n), BIT(n){}
+
+    void update(int idx, int x){
+        for (; idx<n; idx=idx|(idx+1))
+            BIT[idx] += x;
+    }
+
+    int sum(int idx){
+        int ans = 0;
+        for (; idx>= 0; idx=(idx&(idx+1))-1)
+            ans+=BIT[idx];
+        return ans;
+    }
+
+    int sum(int l, int r){
+        return sum(r)-sum(l-1);
+    }
 };
