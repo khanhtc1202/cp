6d1a50c3e9f0267e51ac1edfc5f69614394e550e
khanhtc1202 <khanhtc1202@gmail.com>
2024-09-13 14:46:48 +0900

    Update sol

--- leetcode/prepare/1310.cpp
+++ leetcode/prepare/1310.cpp
@@ -8,12 +8,12 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> res;
-        
+
         // Build prefix XOR array
         for (int i = 1; i < arr.size(); ++i) {
             arr[i] ^= arr[i-1];
         }
-        
+
         // Cal res
         for (const auto& q: queries) {
             if (q[0] > 0) {
