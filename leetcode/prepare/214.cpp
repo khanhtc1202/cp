53163c5f2a5bfb2d0cb565af59cd63647c53aec7
khanhtc1202 <khanhtc1202@gmail.com>
2024-09-20 21:03:31 +0900

    Add sol

---
+++ leetcode/prepare/214.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/shortest-palindrome/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lps[100001];
+    void preKmp(string& s){
+        memset(lps, 0, sizeof(lps));
+        int len=0;
+        const int n=s.size();
+        for(int i=1; i<n; i++){
+            while(len>0 && s[i]!=s[len]) len=lps[len-1];
+            lps[i]=(s[i]==s[len])? ++len: len;
+        }
+    }
+
+    string shortestPalindrome(string& s) {
+        string rev=string(s.rbegin(), s.rend());
+        string pattern=s+'@'+rev;
+        preKmp(pattern);
+        int Len=lps[pattern.size()-1];
+        return rev.substr(0, s.size()-Len)+s;   
+    }
+};
