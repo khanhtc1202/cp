0e1fcc64a634b67bd37192ce9ac46c4fa0abdac7
khanhtc1202 <khanhtc1202@gmail.com>
2024-09-25 09:16:38 +0900

    Update sol

--- leetcode/prepare/2416.cpp
+++ leetcode/prepare/2416.cpp
@@ -4,56 +4,46 @@ Problem desc: https://leetcode.com/problems/sum-of-prefix-scores-of-strings/
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNodeT {
-    int score;
-    vector<TreeNodeT*> nexts;
-
-    TreeNodeT(): score(0), nexts(26) {}
+struct TrieNode {
+    int cnt = 0;
+    TrieNode* next[26] = {};
 };
-
-struct Trie {
-    TreeNodeT* root;
-
-    Trie() {
-        root = new TreeNodeT();
-    }
-
-    void addWord(string word) {
-        TreeNodeT* curr = root;
-        for (auto rune: word) {
-            int idx = rune - 'a';
-            if (curr->nexts[idx] == nullptr) {
-                curr->nexts[idx] = new TreeNodeT();
-            }
-            curr = curr->nexts[idx];
-            curr->score++;
-        }
-    }
-
-    int scoreWord(string word) {
-        TreeNodeT* curr = root;
-        int total = 0;
-        for (auto rune: word) {
-            int idx = rune - 'a';
-            curr = curr->nexts[idx];
-            total += curr->score;
-        }
-        return total;
-    }
-};
-
 
 class Solution {
+    TrieNode root;
 public:
-    vector<int> sumPrefixScores(vector<string>& words) {
-        Trie tree;
-        for (auto word: words) {
-            tree.addWord(word);
+    void insert(string word) {
+        auto node = &root;
+        for (char c: word) {
+            // New prefix
+            if (!node->next[c-'a']) {
+                node->next[c-'a'] = new TrieNode();
+            }
+            // Current prefix count
+            node->next[c-'a']->cnt++;
+            node = node->next[c-'a'];
         }
-        vector<int> ans(words.size(), 0);
-        for (int i = 0; i < words.size(); i++) {
-            ans[i] = tree.scoreWord(words[i]);
+    }
+
+    int count(string s) {
+        auto node = &root;
+        int ans = 0;
+        for (char c: s) {
+            ans += node->next[c-'a']->cnt;
+            node = node->next[c-'a'];
         }
         return ans;
+    }
+
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int N = words.size();
+        for (int i = 0; i < N; ++i) {
+            insert(words[i]);
+        }
+        vector<int> scores(N, 0);
+        for (int i = 0; i < N; ++i) {
+            scores[i] = count(words[i]);
+        }
+        return scores;
     }
 };
