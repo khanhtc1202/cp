a7cb9a2a65e066b11e99677be0469ce6d4f6c67d
khanhtc1202 <khanhtc1202@gmail.com>
2024-10-05 21:45:42 +0700

    Add sol

---
+++ leetcode/prepare/567.cpp
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/permutation-in-string/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void initMap(map<char, int>& m) {
+        if (m.size() != 0) return;
+        string s = "abcdefghijklmnopqrstuvwxyz";
+        for (const auto& c: s) {
+            m[c] = 0;
+        }
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size())
+            return false;
+
+        map<char, int> ms1;
+        initMap(ms1);
+        for (const auto& c: s1)
+            ms1[c]++;
+
+        map<char, int> ms2;
+        initMap(ms2);
+        for (int i = 0; i < s1.size(); i++)
+            ms2[s2[i]]++;
+
+        for (int i = s1.size(); i < s2.size(); i++) {
+            if (matched(ms1, ms2))
+                return true;
+            ms2[s2[i]]++;
+            ms2[s2[i-s1.size()]]--;
+        }
+        return matched(ms1,ms2);
+    }
+
+    bool matched(map<char, int>& ms1, map<char, int>& ms2) {
+        for (const auto& kv: ms1) {
+            if (ms2[kv.first] != ms1[kv.first])
+                return false;
+        }
+        return true;
+    }
+};
