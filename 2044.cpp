a7a812fbca960557901523f2d1c653e816af40f9
khanhtc1202 <khanhtc1202@gmail.com>
2024-10-18 16:54:44 +0700

    Add sol

---
+++ 2044.cpp
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int max = 0;
+        vector<int> dp(1 << 17, 0);
+
+        // Initialize the empty subset
+        dp[0] = 1;
+
+        // Iterate through each number in the input array
+        for (int num : nums) {
+            for (int i = max; i >= 0; i--) {
+                // For each existing subset, create a new subset by including
+                // the current number
+                dp[i | num] += dp[i];
+            }
+            // Update the maximum OR value
+            max |= num;
+        }
+
+        return dp[max];
+    }
+};
