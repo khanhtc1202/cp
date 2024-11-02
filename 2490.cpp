fb1cbb16970813ce85fa366b10c85f9495749b81
khanhtc1202 <khanhtc1202@gmail.com>
2024-11-02 14:24:14 +0700

    Add sol

---
+++ 2490.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/circular-sentence/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == ' ' && s[i-1] != s[i+1])
+                return false;
+        }
+        return s[0] == s[s.size()-1];
+    }
+};
