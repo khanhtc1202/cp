640e121688392d488db38943bd21ea46a47586ff
khanhtc1202 <khanhtc1202@gmail.com>
2024-11-20 17:46:24 +0700

    Add sol

---
+++ 2516.cpp
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        vector<int> count(3, 0);
+        int n = s.size();
+        for (char c: s) count[c-'a']++;
+
+        for (int i = 0; i < 3; ++i)
+            if (count[i] < k) return -1;
+
+        vector<int> window(3, 0);
+        int left = 0, maxWindow = 0;
+        for (int right = 0; right < n; ++right) {
+            window[s[right]-'a']++;
+            // Ensure window is valid by ensure outter has at least k char for each
+            while (left <= right && 
+                (count[0] - window[0] < k || count[1] - window[1] < k ||
+                count[2] - window[2] < k)) {
+                    window[s[left]-'a']--;
+                    left++;
+                }
+            maxWindow = max(maxWindow, right - left + 1);
+        }
+        return n - maxWindow;
+    }
+};
