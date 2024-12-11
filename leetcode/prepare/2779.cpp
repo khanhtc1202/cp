ccff3be6a1f17ee1e47fb4ce05b8597dc4f5cb3b
khanhtc1202 <khanhtc1202@gmail.com>
2024-12-11 17:20:00 +0700

    Update sol

--- leetcode/prepare/2779.cpp
+++ leetcode/prepare/2779.cpp
@@ -18,4 +18,26 @@ public:
         }
         return ans;
     }
+
+    int maximumBeaty_Opt(vector<int>& nums, int k) {
+        if (nums.size() == 1) return 1;
+
+        int maxBeauty = INT_MIN;
+        int maxValue = INT_MIN;
+
+        for (auto x: nums) maxValue = max(maxValue, x);
+
+        vector<int> count(maxValue+1, 0);
+        for (auto x: nums) {
+            count[max(x - k, 0)]++; // add 1 to the start of the range
+            if (x+k+1 <= maxValue) count[x+k+1]--; // subtract 1 to the end of the range
+        }
+
+        int currSum = 0; // represents the current overlaped ranges
+        for (auto val: count) {
+            currSum += val;
+            maxBeauty = max(maxBeauty, currSum);
+        }
+        return maxBeauty;
+    }
 };
