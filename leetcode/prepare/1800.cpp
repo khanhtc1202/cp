a4863a7dd793a4ac3bddea3227dae2212a1d5f91
khanhtc1202 <khanhtc1202@gmail.com>
2025-02-04 18:46:08 +0700

    Add sol

---
+++ leetcode/prepare/1800.cpp
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/maximum-ascending-subarray-sum/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxSum = 0;
+        int currentSubarraySum = nums[0];
+
+        for (int currentIdx = 1; currentIdx < nums.size(); ++currentIdx) {
+            if (nums[currentIdx] <= nums[currentIdx - 1]) {
+                maxSum = max(maxSum, currentSubarraySum);
+                currentSubarraySum = 0;
+            }
+            currentSubarraySum += nums[currentIdx];
+        }
+        return max(maxSum, currentSubarraySum);
+    }
+};
