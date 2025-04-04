3b161ec350d62b4614476135d857ac2d8c3a011c
khanhtc1202 <khanhtc1202@gmail.com>
2025-04-05 00:12:33 +0900

    Add sol

---
+++ leetcode/prepare/1123.cpp
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) {
+            return {nullptr, 0};
+        }
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        }
+        if (left.second < right.second) {
+            return {right.first, right.second + 1};
+        }
+        return {root, left.second + 1};
+    }
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return dfs(root).first;
+    }
+};
