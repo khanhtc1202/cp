9beb84565d7dc19e50c37ed245595447e79b603a
khanhtc1202 <khanhtc1202@gmail.com>
2025-04-20 16:40:13 +0900

    Add sol

---
+++ 781.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/rabbits-in-forest/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int freq[1000]={0}, cnt=0;
+        for(int x: answers){
+            cnt+=(freq[x]++%(x+1)==0)*(x+1);
+        }
+        return cnt;
+    }
+};
