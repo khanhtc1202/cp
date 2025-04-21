fb35c5cb870633cdae9a11417d0798429bacc23d
khanhtc1202 <khanhtc1202@gmail.com>
2025-04-21 18:54:58 +0900

    Add sol

---
+++ 2145.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/count-the-hidden-sequences/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int x = 0, y = 0, cur = 0;
+        for (int d : differences) {
+            cur += d;
+            x = min(x, cur);
+            y = max(y, cur);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+        return (upper - lower) - (y - x) + 1;
+    }
+};
