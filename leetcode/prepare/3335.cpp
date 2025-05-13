80611b1e502010faea8b8e1bbe3a85e4b93a48c5
khanhtc1202 <khanhtc1202@gmail.com>
2025-05-13 19:29:59 +0900

    Update sol

--- leetcode/prepare/3335.cpp
+++ leetcode/prepare/3335.cpp
@@ -29,4 +29,38 @@ public:
         }
         return ans;
     }
+
+    int lengthAfterTransformations_26(string s, int t) {
+        // a -> 26, ab
+        // b -> 26, bc
+        // ...
+        // y -> 26, yz
+
+        // z -> 1, ab
+        // ab -> 24, yz
+        // yz -> 1, zab
+        // z -> 26, zab
+
+        long cnts[26] = {};
+        for (char c : s) ++cnts[c - 'a'];
+        while (t >= 26) {
+            long tmp[26] = {};
+            for (int i = 0; i < 25; ++i) tmp[i + 1] += cnts[i];
+            tmp[0] += cnts[25];
+            tmp[1] += cnts[25];
+            for (int i = 0; i < 26; ++i) {
+                cnts[i] += tmp[i];
+                cnts[i] %= mod;
+            }
+            t -= 26;
+        }
+
+        long ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans += cnts[i];
+            if (i + t >= 26) ans += cnts[i];
+            ans %= mod;
+        }
+        return ans % mod;
+    }
 };
