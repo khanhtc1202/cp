ea3561105bc0fadb3a39f659ca90e0eca6681fbe
khanhtc1202 <khanhtc1202@gmail.com>
2025-06-05 17:57:25 +0900

    Add sol

---
+++ leetcode/prepare/1061.cpp
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/lexicographically-smallest-equivalent-string/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class UnionFind {
+    vector<int> root;
+public:
+    UnionFind(int n):root(n) {
+        iota(root.begin(), root.end(), 0);
+    }
+
+    int Find(int x) { //Path compression
+        return (x==root[x]) ? x : root[x] = Find(root[x]);
+    }
+
+    void Union(int x, int y) {// the smaller one as the common root
+        x= Find(x), y= Find(y);
+        if (x == y) return;
+        else if (x<y) root[y]=x;  
+        else root[x]=y;  
+    }
+};
+
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        UnionFind G(26);
+        const int n = s1.size(), m = baseStr.size();
+        for (int i = 0; i < n; ++i) {
+            G.Union(s1[i] - 'a', s2[i] - 'a');
+        }
+        string ans(m, ' ');
+        for (int i = 0; i < m; ++i) {
+            ans[i] = 'a' + G.Find(baseStr[i] - 'a');
+        }
+        return ans;
+    }
+};
