60b7c0c850d9147c03f901f9b300941c282e31ca
khanhtc1202 <khanhtc1202@gmail.com>
2025-06-08 15:33:14 +0900

    Add sol

---
+++ leetcode/prepare/2434.cpp
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string robotWithString(string s) {
+        unordered_map<char, int> cnt;
+        for (char c : s) {
+            cnt[c]++;
+        }
+
+        stack<char> stk;
+        string res;
+        char minCharacter = 'a';
+        for (char c : s) {
+            stk.emplace(c);
+            cnt[c]--;
+            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+                minCharacter++;
+            }
+            while (!stk.empty() && stk.top() <= minCharacter) {
+                res.push_back(stk.top());
+                stk.pop();
+            }
+        }
+
+        return res;
+    }
+};
